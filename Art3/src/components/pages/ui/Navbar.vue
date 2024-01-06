fc40a38d7f62fbb2289709d513ddfa85b00b99a7
Dmitry <d555601@yandex.ru>
2024-01-06 16:52:49 +0300

    Update Navbar.vue

--- Art3/src/components/pages/ui/Navbar.vue
+++ Art3/src/components/pages/ui/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar-container">
+  <div :class="{ 'navbar-container': true, 'navbar-fixed': isNavbarFixed }">
   <img src="../../../assets//img/logoNataAvodes.png" class="logo_navbar" alt="logo" />
   <ul class="nav nav-underline">
    <li class="nav-item">
@@ -16,6 +16,31 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      isNavbarFixed: false
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const scrollY = window.scrollY;
+
+      // При скролле на 100px меняем состояние фиксации и цвета фона
+      if (scrollY > 100) {
+        this.isNavbarFixed = true;
+      } else {
+        this.isNavbarFixed = false;
+      }
+    }
+  }
+};
 </script>
 
 <style>
