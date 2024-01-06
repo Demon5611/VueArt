79e99f4fb3f25ca5246c5d7aa85013781f965e32
Demon5611 <d555601@ya.ru>
2024-01-06 14:06:42 +0300

    first commit

---
+++ Art3/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/recommended',
+      'plugin:parcel/recommended',
+    ],
+    rules: {
+        quotes: ['error', 'single'],
+    },
+  };
---
+++ Art3/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+  <Navbar/>
+  <div>hello</div>
+</div>
+</template>
+
+<script setup>
+
+import Navbar from './components/pages/ui/Navbar.vue';
+</script>
+
+<style>
+.app {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  line-height: 1.5;
+  font-weight: 400;
+  background-color: rgb(171, 169, 197);
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+  font-size: 20px;
+  font-family: 'Montserrat', sans-serif;
+  text-align: center;
+}
+</style>
