2d7037ce0131b28d9a37c2076a2e79f7c55b8127
Dmitry <d555601@yandex.ru>
2024-01-10 17:13:30 +0300

    Update Navbar.vue

--- Art3/src/components/pages/ui/Navbar.vue
+++ Art3/src/components/pages/ui/Navbar.vue
@@ -46,3 +46,31 @@ onUnmounted(() => {
 
 <style>
 </style>
+
+<!-- <script lang="js">
+export default {
+  data() {
+    return {
+      isNavbarFixed: false
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const scrollY = window.scrollY;
+
+      // При скролле на 100px меняем состояние фиксации и цвета фона
+      if (scrollY > 100) {
+        this.isNavbarFixed = true;
+      } else {
+        this.isNavbarFixed = false;
+      }
+    }
+  }
+};
+</script> -->
