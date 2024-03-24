df1ac546814fcb120b3b69d3174de56cc694cb49
Седов Дмитрий <d555601@gmail.com>
2024-03-24 21:45:38 +0300

    adaptiv style is finished

--- Art3/src/components/pages/ui/Navbar.vue
+++ Art3/src/components/pages/ui/Navbar.vue
@@ -2,16 +2,16 @@
   <div :class="{ 'navbar-container': true, 'navbar-fixed': isNavbarFixed }">
     <img src="../../../assets/logoNataAvodes.png" class="logo_navbar" alt="logo" />
     <ul class="nav nav-underline">
-      <li class="nav-item">
-        <!-- якорная ссылка, где #main это id="main" на Mainpage.vue -->
-        <a class="nav-link" href="#main">Главная</a> 
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#gallery">Галерея</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#contacts">Контакты</a>
-      </li>
+          <li class="nav-item">
+            <!-- якорная ссылка, где #main это id="main" на Mainpage.vue -->
+            <a class="nav-link" href="#main">Главная</a> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#gallery">Галерея</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contacts">Контакты</a>
+          </li>
     </ul>
   </div>
 </template>
