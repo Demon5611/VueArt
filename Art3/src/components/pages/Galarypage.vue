e1678291f570b658a3396eb1815a03dd5ba24a19
Седов Дмитрий <d555601@gmail.com>
2024-03-24 22:47:08 +0300

    add click on foto and open at new page

--- Art3/src/components/pages/Galarypage.vue
+++ Art3/src/components/pages/Galarypage.vue
@@ -1,7 +1,11 @@
   <template>
     <div >
       <div class="textRight1">
-        <img src="../../assets/monky1.jpeg" class="card" @click="openModalWindow" />
+      
+          <!-- <img src="../../assets/monky1.jpeg" class="card" @click="openModalWindow" /> -->
+          <img src="../../assets/monky1.jpeg" class="card"  @click="openImage(imageUrl.monky1)"/>
+
+    
         <ModalWindow :imageSrc="imageSrc" :isModalOpen="isModalOpen" @close="closeModal" />
         <div class="textAfterCard1">
           <h4 class="h1_1">ПРОЦЕСС ЭВОЛЮЦИИ</h4>
@@ -16,7 +20,9 @@
       </div>
 
       <div class="textRight2">
-        <img src="../../assets/interior.jpg" class="card" />
+    
+          <img src="../../assets/interior.jpg" class="card" @click="openImage(imageUrl.interior)"/>
+      
         <div class="textAfterCard2">
           <h4 class="h2_2">КОРОЛЕВ</h4>
           <p>
@@ -30,7 +36,9 @@
       </div>
 
       <div class="textRight3">
-        <img src="../../assets/interior.jpg" class="card" />
+        
+          <img src="../../assets/interior.jpg" class="card" @click="openImage(imageUrl.interior)"/>
+    
         <div class="textAfterCard3">
           <h4 class="h3_3">МИГРАЦИОННЫЙ ЗАХВАТ</h4>
           <p>
@@ -51,10 +59,15 @@
   <script setup>
   import { computed, ref, watchEffect } from 'vue';
 import NataAvodesCV from '../../assets/NataAvodesCV.pdf';
-import imageSrc from '../../assets/interior.jpg';
+import { default as imageSrc, default as interior } from '../../assets/interior.jpg';
+import monky1 from '../../assets/monky1.jpeg';
 import ModalWindow from './ui/ModalWindow.vue';
 
+  const imageUrl = {
+  interior,
+  monky1,
 
+}
   const handleDownload = () => {
     const link = document.createElement('a');
     link.href = NataAvodesCV;
@@ -64,6 +77,11 @@ import ModalWindow from './ui/ModalWindow.vue';
     link.click();
     document.body.removeChild(link);
   };
+
+  const openImage = (imageUrl) => {
+  // Открывайте изображение в новом окне или модальном окне, как вам угодно.
+  window.open(imageUrl, '_blank');
+};
 
   const isModalOpen = ref(false);
 
