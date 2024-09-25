4f9cf3d7737613973919ad03786252ac6739cf75
Demon5611 <d555601@gmail.com>
2024-09-25 14:07:41 +0300

    refactor1

---
+++ Art3/src/components/ImageCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="textRight">
+      <img :src="imageUrl" class="card" @click="openImage(imageUrl)" />
+      <div class="textAfterCard">
+        <h4>{{ title }}</h4>
+        <p>{{ description }}</p>
+        <p class="material">{{ material }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    imageUrl: String,
+    title: String,
+    description: String,
+    material: String,
+  });
+  
+  const openImage = (imageUrl) => {
+    window.open(imageUrl, '_blank');
+  };
+  </script>
+
+  
--- Art3/src/components/pages/Galarypage.vue
+++ Art3/src/components/pages/Galarypage.vue
@@ -1,68 +1,58 @@
-  <template>
-    <div id="gallery">
-      <div class="textRight1">
-          <img src="../../assets/monky1.jpeg" class="card"  @click="openImage(imageUrl.monky1)"/>
-            <div class="textAfterCard1">
-          <h4 class="h1_1">ПРОЦЕСС ЭВОЛЮЦИИ</h4>
-          <p>
-            Это яркий вызов современной реальности. Нет ничего хуже интеллектуальных шорт,
-            навязанных нам….Кем навязанных? Социальными предрассудками, бизнесом и личными
-            интересами отдельных групп людей. Каждая картина заставляет задуматься о том, что делают
-            люди КАЖДЫЙ ДЕНЬ. Мы на пути эволюции или деградации как вида? Решать тебе…
-          </p>
-          <p class="material">Материал: смешанная техника, графика. Размеры работ 80/100см</p>
-        </div>
-      </div>
+<template>
+  <div id="gallery">
+    <ImageCard
+      :imageUrl="imageUrl.monky1"
+      title="ПРОЦЕСС ЭВОЛЮЦИИ"
+      description="Это яркий вызов современной реальности. Нет ничего хуже интеллектуальных шорт, навязанных нам….Кем навязанных? Социальными предрассудками, бизнесом и личными интересами отдельных групп людей. Каждая картина заставляет задуматься о том, что делают люди КАЖДЫЙ ДЕНЬ. Мы на пути эволюции или деградации как вида? Решать тебе…"
+      material="Материал: смешанная техника, графика. Размеры работ 80/100см"
+      @openImage="memoizedOpenImage"
+    />
 
-      <div class="textRight2">
-    
-          <img src="../../assets/interior.jpg" class="card" @click="openImage(imageUrl.interior)"/>
-      
-        <div class="textAfterCard2">
-          <h4 class="h2_2">КОРОЛЕВ</h4>
-          <p>
-            Это яркий вызов современной реальности. Нет ничего хуже интеллектуальных шорт,
-            навязанных нам….Кем навязанных? Социальными предрассудками, бизнесом и личными
-            интересами отдельных групп людей. Каждая картина заставляет задуматься о том, что делают
-            люди КАЖДЫЙ ДЕНЬ. Мы на пути эволюции или деградации как вида? Решать тебе…
-          </p>
-          <p class="material">Материал: масло, пастель; графика. Размеры работ 30/40см</p>
-        </div>
-      </div>
+    <ImageCard
+      :imageUrl="imageUrl.interior"
+      title="КОРОЛЕВ"
+      description="Это яркий вызов современной реальности. Нет ничего хуже интеллектуальных шорт, навязанных нам….Кем навязанных? Социальными предрассудками, бизнесом и личными интересами отдельных групп людей. Каждая картина заставляет задуматься о том, что делают люди КАЖДЫЙ ДЕНЬ. Мы на пути эволюции или деградации как вида? Решать тебе…"
+      material="Материал: масло, пастель; графика. Размеры работ 30/40см"
+      @openImage="memoizedOpenImage"
+    />
 
-      <div class="textRight3">
-        
-          <img src="../../assets/interior.jpg" class="card" @click="openImage(imageUrl.interior)"/>
-    
-        <div class="textAfterCard3">
-          <h4 class="h3_3">МИГРАЦИОННЫЙ ЗАХВАТ</h4>
-          <p>
-            Это яркий вызов современной реальности. Нет ничего хуже интеллектуальных шорт,
-            навязанных нам….Кем навязанных? Социальными предрассудками, бизнесом и личными
-            интересами отдельных групп людей. Каждая картина заставляет задуматься о том, что делают
-            люди КАЖДЫЙ ДЕНЬ. Мы на пути эволюции или деградации как вида? Решать тебе…
-          </p>
-          <p class="material">
-            Материал: цифровая графика, коллаж, печать на пенокартоне. Размеры работ 80/120см
-          </p>
-          <a @click="handleDownload" class="cv">Скачать NataAvodesCV</a>
-        </div>
-      </div>
-    </div>
-  </template>
+    <ImageCard
+      :imageUrl="imageUrl.interior"
+      title="МИГРАЦИОННЫЙ ЗАХВАТ"
+      description="Это яркий вызов современной реальности. Нет ничего хуже интеллектуальных шорт, навязанных нам….Кем навязанных? Социальными предрассудками, бизнесом и личными интересами отдельных групп людей. Каждая картина заставляет задуматься о том, что делают люди КАЖДЫЙ ДЕНЬ. Мы на пути эволюции или деградации как вида? Решать тебе…"
+      material="Материал: цифровая графика, коллаж, печать на пенокартоне. Размеры работ 80/120см"
+      @openImage="memoizedOpenImage"
+    />
 
-  <script setup>
-  import NataAvodesCV from '../../assets/NataAvodesCV.pdf';
-  import { default as interior } from '../../assets/interior.jpg';
-  import monky1 from '../../assets/monky1.jpeg';
+    <a @click="handleCachedDownload" class="cv">Скачать NataAvodesCV</a>
+  </div>
+</template>
 
+<script setup>
+import ImageCard from '../ImageCard.vue';
+import NataAvodesCV from '../../assets/NataAvodesCV.pdf';
+import { default as interior } from '../../assets/interior.jpg';
+import monky1 from '../../assets/monky1.jpeg';
+import memoize from 'lodash/memoize'; // Используем lodash для мемоизации
 
-  const imageUrl = {
+const imageUrl = {
   interior,
   monky1,
+};
 
-}
-  const handleDownload = () => {
+// Мемоизация функции открытия изображения
+const openImage = (url) => {
+  console.log('Открытие изображения:', url);
+  window.open(url, '_blank');
+};
+
+// Используем lodash для мемоизации функции
+const memoizedOpenImage = memoize(openImage);
+
+// Кеширование скачивания файла
+let isFileCached = false;
+const handleCachedDownload = () => {
+  if (!isFileCached) {
     const link = document.createElement('a');
     link.href = NataAvodesCV;
     link.target = '_blank';
@@ -70,9 +60,9 @@
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  };
-
-  const openImage = (imageUrl) => {
-  window.open(imageUrl, '_blank');
+    isFileCached = true; // Помечаем, что файл был скачан
+  } else {
+    console.log('Файл уже скачан');
+  }
 };
-  </script>
+</script>
