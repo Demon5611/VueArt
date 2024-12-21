74f6ece5ed8236ef634b8b201497a2125c87ea9c
Дмитрий Седов <dmitrijsedov@MacBook-Pro-Dmitrij.local>
2024-12-21 23:51:02 +0300

    tune index-html

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/favicon.svg', 
+          dest: 'assets' 
+        },
+        {
+          src: 'src/assets/favicon.png',
+          dest: 'assets' 
+        }
+      ]
+    })
+  ],
+  server: {
+    port: 3010,
+    host: true,
+  },
   base: './',
   build: {
-    outDir: 'dist' 
-  }
+    outDir: 'dist',
+  },
 });
