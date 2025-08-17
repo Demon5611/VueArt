26da17adaa7ac0679252b8abb81c1dd144edc8a3
Dmitriy Sedov <sedov.dmitriy19@pf5evzch.foreman-test.vm.prod-5.cloud.dataline>
2025-08-17 21:40:31 +0300

    change styles

--- src/components/pages/GalaryDetails.vue
+++ src/components/pages/GalaryDetails.vue
@@ -17,9 +17,7 @@
         <router-link to="/contactpage" class="buy-button"
           >Связаться</router-link
         >
-                <router-link to="/basket" class="basket-button"
-          >Купить</router-link
-        >
+        <router-link to="/basket" class="basket-button">Купить</router-link>
         <p class="description">
           {{ foto.description.split(". ")[0] }}
           <br v-if="foto.description.includes('. ')" />
@@ -72,102 +70,3 @@ onMounted(() => {
 });
 </script>
 
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  max-width: 1100px;
-  padding: 5%;
-  margin: 0 auto;
-  background-color: #fff;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.foto {
-  display: flex;
-  align-items: flex-start;
-  gap: 20px;
-  margin-bottom: 30px;
-}
-
-/* Для нечётных карточек — row-reverse */
-.foto:nth-child(odd) {
-  flex-direction: row-reverse;
-}
-
-.foto-image {
-  width: 450px;
-  height: 450px;
-  object-fit: cover;
-  border-radius: 2px;
-}
-
-.foto-info {
-  display: flex;
-  flex-direction: column;
-  max-width: 400px;
-  align-items: flex-start;
-}
-
-h2 {
-  font-size: 1.5rem;
-  color: #000;
-}
-
-.price {
-  font-weight: bold;
-  font-size: 1.2rem;
-  margin: 5px 0;
-}
-
-.buy-button {
-  max-width: 200px;
-  background-color: #ffb24d;
-  color: white;
-  border: none;
-  padding: 10px 15px;
-  font-size: 1rem;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-bottom: 10px;
-  text-decoration: none;
-}
-
-.basket-button{
-  max-width: 200px;
-  background-color: #ff684d;
-  color: white;
-  border: none;
-  padding: 10px 15px;
-  font-size: 1rem;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-bottom: 10px;
-  text-decoration: none;
-}
-
-.buy-button:hover {
-  background-color: #d93737;
-}
-
-.description {
-  color: #333;
-  font-size: 1rem;
-  line-height: 1.5;
-  text-align: left;
-}
-
-/* Адаптив для экранов меньше 768px */
-@media (max-width: 768px) {
-  .foto {
-    flex-direction: column !important;
-    text-align: center;
-  }
-
-  .foto-image {
-    width: 100%;
-    height: auto;
-  }
-}
-</style>
